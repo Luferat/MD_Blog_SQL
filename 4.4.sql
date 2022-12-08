928c304f7262eddba95e54e94359665a41dfb2a9
André Luferat <33375030+Luferat@users.noreply.github.com>
2022-12-08 20:00:31 -0300

    Update 4.4.sql

--- 4.4.sql
+++ 4.4.sql
@@ -2,17 +2,13 @@
 -- autor do artigo em um campo "fullname".
 
 SELECT *,
-	TIMESTAMPDIFF(YEAR, birth, NOW()) AS age,
-    DATE_FORMAT(art_date, "%d/%m/%Y às %H:%i") AS art_date_br,
-    -- Contatena as partes do nome do autor e armazena em "fullname".
-    CONCAT(
-        -- Extrai primeiro nome do autor.
-    	SUBSTRING_INDEX(name, ' ', 1),
-        -- Adiciona um espaço entre os nomes.
-    	' ',
-        -- Extrai o último nome do autor.
-    	SUBSTRING_INDEX(name, ' ', -1)
-	) AS fullname
+    TIMESTAMPDIFF(YEAR, birth, NOW()) AS age,
+    DATE_FORMAT(art_date, "%d/%m/%Y às %H:%i") AS art_date_br,    
+    CONCAT(                            -- Contatena as partes do nome do autor e armazena em "fullname".
+        SUBSTRING_INDEX(name, ' ', 1), -- Extrai primeiro nome do autor.
+        ' ',                           -- Adiciona um espaço entre os nomes.
+    	SUBSTRING_INDEX(name, ' ', -1) -- Extrai o último nome do autor.
+    ) AS fullname
 FROM articles
 INNER JOIN users ON user_id = author
 WHERE art_id = '2' 
